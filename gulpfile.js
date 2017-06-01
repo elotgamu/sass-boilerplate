5ef130cc46f9dcce1c9af7b5470e188377aee1a7
Eleazar Gaitan <elotgamu@gmail.com>
2017-05-31 22:45:28 -0600

    Fixed the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var dev = true;
 
 gulp.task('styles', () => {
   // return gulp.src('app/styles/*.scss')
-  return gulp.src('scss/**/*.scss')
+  return gulp.src('stylesheets/**/*.scss')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync({
@@ -28,159 +28,35 @@ gulp.task('styles', () => {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('scripts', () => {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/scripts'))
-    .pipe(reload({stream: true}));
+/* Reload task */
+gulp.task('bs-reload', function () {
+    browserSync.reload();
 });
 
-function lint(files, options) {
-  return gulp.src(files)
-    .pipe($.eslint({ fix: true }))
-    .pipe(reload({stream: true, once: true}))
-    .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
-}
-
-gulp.task('lint', () => {
-  return lint('app/scripts/**/*.js')
-    .pipe(gulp.dest('app/scripts'));
-});
-gulp.task('lint:test', () => {
-  return lint('test/spec/**/*.js')
-    .pipe(gulp.dest('test/spec'));
-});
-
-gulp.task('html', ['styles', 'scripts'], () => {
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
-});
-
-gulp.task('extras', () => {
-  return gulp.src([
-    'app/*',
-    '!app/*.html'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
-
-gulp.task('serve', () => {
-  /* runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {*/
-  /*runSequence([['styles'], () => {
-    browserSync.init({
-      notify: false,
-      port: 9000,
-      server: {
-        baseDir: ['.tmp', 'app'],
-        baseDir: ['.'],
-        routes: {
-          '/bower_components': 'bower_components'
+/* Prepare Browser-sync for localhost */
+gulp.task('browser-sync', function() {
+    browserSync.init(['css/*.css'], {
+        /*
+        I like to use a vhost, WAMP guide: https://www.kristengrote.com/blog/articles/how-to-set-up-virtual-hosts-using-wamp, XAMP guide: http://sawmac.com/xampp/virtualhosts/
+        */
+        // proxy: 'your_dev_site.url'
+        /* For a static server you would use this: */
+        /*
+        */
+        server: {
+            baseDir: './'
         }
-      }
-    });*/
-    browserSync.init({
-      notify: false,
-      port: 9000,
-      server: {
-        baseDir: ['./'],
-        /*routes: {
-          '/bower_components': 'bower_components'
-        }*/
-      }
+
     });
-
-
-
-    //gulp.watch('app/styles/**/*.scss', ['styles']);
-    gulp.watch('scss/**/*.scss', ['styles']);
-    // gulp.watch('app/scripts/**/*.js', ['scripts']);
-    // gulp.watch('app/fonts/**/*', ['fonts']);
-    // gulp.watch('bower.json', ['wiredep', 'fonts']);
-    gulp.watch([
-      //'app/*.html',
-      '*.html',
-      'scss/**/*.scss',
-      // 'app/images/**/*',
-      // '.tmp/fonts/**/*'
-    ]).on('change', reload);
 });
 
-gulp.task('serve:dist', ['default'], () => {
-  browserSync.init({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['dist']
-    }
-  });
-});
 
-gulp.task('serve:test', ['scripts'], () => {
-  browserSync.init({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/scripts': '.tmp/scripts',
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('app/scripts/**/*.js', ['scripts']);
-  gulp.watch(['test/spec/**/*.js', 'test/index.html']).on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
-});
-
-// inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
-    .pipe($.filter(file => file.stat && file.stat.size))
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
-
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
-
-gulp.task('default', () => {
-  return new Promise(resolve => {
-    dev = false;
-    runSequence(['clean', 'wiredep'], 'build', resolve);
-  });
+/* Watch scss, js and html files, doing different things with each. */
+gulp.task('serve', ['styles', 'browser-sync'], function () {
+    /* Watch scss, run the sass task on change. */
+    gulp.watch(['stylesheets/*.scss', 'stylesheets/**/*.scss'], ['styles'])
+    /* Watch app.js file, run the scripts task on change. */
+    /* gulp.watch(['js/app.js'], ['scripts']) */
+    /* Watch .html files, run the bs-reload task on change. */
+    gulp.watch(['*.html'], ['bs-reload']);
 });
